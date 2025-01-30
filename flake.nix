6b20117f51acfcabbe0d6d6bcb5c101c58a8b69d
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-01-30 14:43:46 -0600

    add dev shell to flake

--- flake.nix
+++ flake.nix
@@ -109,6 +109,26 @@
               EOF
             '';
           };
+
+          devShells.default = pkgs.mkShell {
+            name = "yapper";
+            buildInputs = [
+              pkgs.python312
+              whisperWithCuda
+              pkgs.wtype
+              pkgs.gtk4
+              pkgs.gobject-introspection
+              pkgs.gtk4.dev
+              pkgs.python312Packages.pygobject3
+            ];
+            
+            shellHook = ''
+              echo "Welcome to the yapper development environment!"
+              echo "Python 3.12 and all necessary libraries for Whisper, GTK, and ydotool are ready to go."
+              echo "CUDA support is enabled for whisper-cpp using the system-wide CUDA installation"
+              zsh
+            '';
+          };
         };
     };
 }
